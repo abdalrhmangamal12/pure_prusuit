27ed0775ac2e59ff2c6aec4dcfdad63465bc0a8a
abdo <abdalrahmangamal1999@gmail.com>
2025-01-16 10:44:06 +0200

    add calculation for robot state

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include "../include/ppcontroller.hpp"
 #include<vector>
+#include<cmath>
 #include<chrono>
 #include<thread>
+#include<iostream>
 int main(){
     pure_prusuit pp_localplanner;
     pp_localplanner.set_robot_pose({0.0,0.0,0.0,0.0,0.0,0.0});
@@ -12,16 +14,18 @@ int main(){
                                           {4,4,0,0,0,0},
                                           {5,5,0,0,0,0}};
     pp_localplanner.set_path(path);
-    int sim_steps = 50;
+    int sim_steps = 1000;
     pure_prusuit::point current_state;
      std::chrono::milliseconds sim_step(100);
     for(int i =0 ; i<=sim_steps;i++){
       
     pure_prusuit::velocity current_vel=pp_localplanner.compute_velocity();
-    current_state.pos_x=current_state.pos_x + 0.1*current_vel.linear_vel;
-    current_state.pos_y=current_state.pos_y + 0.1*current_vel.linear_vel;
-    pp_localplanner.set_robot_pose({current_state.pos_x,current_state.pos_y,0.0,0.0,0.0,0.0});
-
+    current_state.pos_x+=current_vel.linear_vel*std::cos(current_state.rot_z)*0.1;
+    current_state.pos_y+=current_vel.linear_vel*std::sin(current_state.rot_z)*0.1;
+    current_state.rot_z+=current_vel.angular_vel*0.1;
+    std::cout<<"current x :"<<current_state.pos_x<<"current y is :"<<current_state.pos_y<<std::endl;
+    pp_localplanner.set_robot_pose({current_state.pos_x,current_state.pos_y,0.0,0.0,0.0,current_state.rot_z});
+   
     
      std::this_thread::sleep_for(sim_step);
     }
